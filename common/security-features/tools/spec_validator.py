2076b126bf5c14e3519110d7524ed83e7e53c6f9
Hiroshige Hayashizaki <hiroshige@chromium.org>
2019-07-23 15:08:19 -0700

    [WPT/common/security-features] Support nested source contexts
    This CL adds more flexible support for requests from
    nested contexts.
    
    spec.src.json is modified for the following reasons:
    
    [A] Make names more neutral from specific tests (e.g. referrer policy)
        and normalize structures.
    [B] Reflect names/structure changes by preceding refactoring.
        Before this CL, these names are remapped in e.g.
        referrer-policy-test-case.sub.js.
    [C] Add supports for source contexts, to enable more flexible
        testing in workers, nested browsing contexts, etc.
    
    After this CL, `source_context_schema` controls what kind of
    source contexts are used and in which context should some
    policies be applied.
    Invalid delivery types (combined with source contexts and
    subresource types, e.g. `attr` for Workers) are filtered out by
    newly introduced `delivery_type_schema` [C].
    
    Changes in `test_expansion` entries and `test_expansion_schema`:
    - `target_protocol` and `origin` fields are merged into a single
      `origin` fields [B].
    - `delivery_type` fields are renamed from `delivery_method` and
      the values are changed to "*" [C].
    - `delivery_value` fields are renamed from `referrer_policy` field [A] and
      moved from parent `specification` entries.
    - `expected` fields are renamed from `referrer_url` [A].
    - `source_context` fields are added [C].
    - `referrer_policy_schema` is moved to `delivery_value`
      inside `test_expansion_schema` [A].
    
    Other changes:
    - `elements-not-supporting-attr-referrer` and
      `elements-not-supporting-rel-noreferrer` are removed [C].
    - `name` fields that don't add much information are removed [A].
    
    Change-Id: Ia6695081e5a93355c29fb1bdb7e89ba6b673261f

--- common/security-features/tools/spec_validator.py
+++ common/security-features/tools/spec_validator.py
@@ -3,7 +3,7 @@
 from __future__ import print_function
 
 import json, sys
-from common_paths import *
+
 
 def assert_non_empty_string(obj, field):
     assert field in obj, 'Missing field "%s"' % field
@@ -31,12 +31,13 @@ def assert_contains(obj, field):
 
 
 def assert_value_from(obj, field, items):
-   assert obj[field] in items, \
-        'Field "%s" must be from: %s' % (field, str(items))
+    assert obj[field] in items, \
+         'Field "%s" must be from: %s' % (field, str(items))
 
 
 def assert_atom_or_list_items_from(obj, field, items):
-    if isinstance(obj[field], basestring) or isinstance(obj[field], int):
+    if isinstance(obj[field], basestring) or isinstance(
+            obj[field], int) or obj[field] is None:
         assert_value_from(obj, field, items)
         return
 
@@ -71,13 +72,15 @@ def assert_valid_artifact(exp_pattern, artifact_key, schema):
         assert_valid_artifact(exp_pattern[artifact_key], sub_artifact_key,
                               sub_schema)
 
+
 def validate(spec_json, details):
     """ Validates the json specification for generating tests. """
 
     details['object'] = spec_json
-    assert_contains_only_fields(spec_json, ["specification",
-                                            "test_expansion_schema",
-                                            "excluded_tests"])
+    assert_contains_only_fields(spec_json, [
+        "specification", "delivery_key", "delivery_type_schema", "source_context_schema",
+        "test_expansion_schema", "excluded_tests"
+    ])
     assert_non_empty_list(spec_json, "specification")
     assert_non_empty_dict(spec_json, "test_expansion_schema")
     assert_non_empty_list(spec_json, "excluded_tests")
@@ -93,12 +96,9 @@ def validate(spec_json, details):
         details['object'] = spec
 
         # Validate required fields for a single spec.
-        assert_contains_only_fields(spec, ['name',
-                                           'title',
-                                           'description',
-                                           'specification_url',
-                                           'test_expansion'])
-        assert_non_empty_string(spec, 'name')
+        assert_contains_only_fields(
+            spec,
+            ['title', 'description', 'specification_url', 'test_expansion'])
         assert_non_empty_string(spec, 'title')
         assert_non_empty_string(spec, 'description')
         assert_non_empty_string(spec, 'specification_url')
@@ -123,14 +123,11 @@ def validate(spec_json, details):
 
     # Validate the test_expansion schema members.
     details['object'] = test_expansion_schema
-    assert_contains_only_fields(test_expansion_schema, ['expansion',
-                                                        'source_scheme',
-                                                        'opt_in_method',
-                                                        'context_nesting',
-                                                        'redirection',
-                                                        'subresource',
-                                                        'origin',
-                                                        'expectation'])
+    assert_contains_only_fields(test_expansion_schema, [
+        'expansion', 'delivery_type', 'delivery_value', 'redirection',
+        'origin', 'source_protocol', 'source_context', 'subresource',
+        'expected'
+    ])
     # Validate excluded tests.
     details['object'] = excluded_tests
     for excluded_test_expansion in excluded_tests:
@@ -139,10 +136,8 @@ def validate(spec_json, details):
         details['object'] = excluded_test_expansion
         for artifact in test_expansion_schema:
             details['test_expansion_field'] = artifact
-            assert_valid_artifact(
-                excluded_test_expansion,
-                artifact,
-                test_expansion_schema[artifact])
+            assert_valid_artifact(excluded_test_expansion, artifact,
+                                  test_expansion_schema[artifact])
             del details['test_expansion_field']
 
     del details['object']
@@ -159,7 +154,7 @@ def assert_valid_spec_json(spec_json):
 
 
 def main():
-    spec_json = load_spec_json();
+    spec_json = load_spec_json()
     assert_valid_spec_json(spec_json)
     print("Spec JSON is valid.")
 
